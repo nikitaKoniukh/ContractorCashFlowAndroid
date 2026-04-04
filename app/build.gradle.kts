1fb3556f70b94d379523f11e420f35d7496ebfb2
Nikita Koniukh <nikita-k@yit.co.il>
2026-04-04 09:46:27 +0300

    version 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.yetzira.ContractorCashFlowAndroid"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
 
